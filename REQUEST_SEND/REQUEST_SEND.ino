06f804df73b179060dc74ff955b0d7ef40bd5d6d
Buddhima <buddhimah@millenniumit.com>
2017-11-14 18:52:57 +0530

    ThisCommitContainsCode

---
+++ REQUEST_SEND/REQUEST_SEND.ino
@@ -0,0 +1,71 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266Ping.h>
+#include <WiFiClientSecure.h>
+#include <ESP8266Ping.h>
+#include <PubSubClient.h> 
+const char* ssid     = "SSID";
+const int httpsPort = 443;
+
+const char* url="https://url";
+
+String data="username=####&password=####&buttonClicked=4";
+
+
+
+const char* host = "host";
+
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+   pubclient.setClient(wclient);
+  pubclient.setServer(mqtt_server, mqtt_port);
+
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid);
+
+ while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP()); 
+  // Use WiFiClientSecure class to create TLS connection
+  WiFiClientSecure client;
+  Serial.print("connecting to ");
+  Serial.println(host);
+  if (!client.connect(host, httpsPort)) {
+    Serial.println("connection failed");
+    return;
+  }
+   client.print(String("POST ") + url + " HTTPS/1.1\r\n" +
+                 "Host: " + host + "\r\n" +
+                 "Connection: close\r\n" +
+                 "Content-Type: application/x-www-form-urlencoded\r\n" +
+                 "Content-Length: " + data.length() + "\r\n" +
+                 "\r\n" + // This is the extra CR+LF pair to signify the start of a body
+                 data + "\n");
+    //Delay
+    delay(1000);
+
+     bool ret = Ping.ping("iot.eclipse.org");
+        Serial.println(ret);
+
+
+       
+
+
+ 
+
+}
+
+void loop() {
+
+}
